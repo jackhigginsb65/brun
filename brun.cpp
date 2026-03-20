2c2f7870e0bbb353226f5a6dba111641d3072651
Jack Higgins <jackhigginsb65@gmail.com>
2026-03-19 21:50:23 -0700

    Decreased file size

--- brun.cpp
+++ brun.cpp
@@ -70,27 +70,3 @@ int main(int argc, char* argv[]) {
 	}
 	brunprocess(del, conly, filename, output);
 }
-	/* std::string compile_cmd;
-
-	if (filename.find(".cpp") != std::string::npos) {
-		compile_cmd = "g++ " + filename + " -o" + output;
-	} else if (filename.find(".c") != std::string::npos) {
-		compile_cmd = "gcc " + filename + " -o" + output;
-	} else {
-		std::cout << "brun: Source-code is not a C/C++ file\n";
-		return 1;
-	}
-
-	std::cout << "brun: Compiling...\n";
-	int compile_result = system(compile_cmd.c_str());
-
-	if (compile_result != 0) {
-		std::cout << "brun: Compilation failed.\n";
-		return 1;
-	}
-
-	std::cout << "brun: Running...\n\n";
-	system(("./" + output).c_str());
-
-	return 0; */
-
