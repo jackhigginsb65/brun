ad41edc2bcab917c3f4507dbd706817abb06ccc7
Jack Higgins <jackhigginsb65@gmail.com>
2026-03-19 21:42:31 -0700

    Added flags -c and -d, better error responses

--- brun.cpp
+++ brun.cpp
@@ -2,16 +2,75 @@
 #include <string>
 #include <cstdlib>
 
+int brunprocess(bool del, bool conly, const std::string& filename, std::string& output) {
+	std::string compile_cmd;
+
+	if (filename.find(".cpp") != std::string::npos) {
+		compile_cmd = "g++ " + filename + " -o" + output;
+	} else if (filename.find(".c") != std::string::npos) {
+		compile_cmd = "gcc " + filename + " -o" + output;
+	} else {
+		std::cout << "brun: Source-code is not a C/C++ file\n";
+		return 1;
+	}
+
+	std::cout << "brun: Compiling...\n";
+	int compile_result = system(compile_cmd.c_str());
+
+	if (compile_result != 0) {
+		std::cout << "brun: Compilation failed.\n";
+		return 1;
+	}
+
+	if (del == true) {
+		std::cout << "brun: Running...\n";
+		system(("./" + output).c_str());
+		system(("rm " + output).c_str());
+		return 0;
+	}
+	if (conly == false) {
+		system(("./" + output).c_str());
+	}
+	return 0;
+}
+
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
 		std::cout << "brun-Usage: brun <file.c/cpp>\n";
 		return 0;    
 	}
-	
-	std::string filename = argv[1];
-	std::string output = filename.substr(0, filename.find('.'));
 
-	std::string compile_cmd;
+	bool del = false;
+	bool conly = false;
+
+	std::string filename;
+
+	for (int i = 1; i < argc; i++) {
+		std::string currentArg = argv[i];
+
+		if (currentArg.front() != '-') {
+			filename = currentArg;
+		} else {
+			if (currentArg == "-c" || currentArg == "-C") {
+				conly = true;
+			}
+			if (currentArg == "-d" || currentArg == "-D") {
+				del = true;
+			}
+		}
+	}
+
+	std::string output;
+
+	if (filename.find(".") == std::string::npos) {
+		std::cout << "brun: Error; Invalid filename '" << filename << "'. Check spelling?\n";
+		return 1;
+	} else {
+		output = filename.substr(0, filename.find('.'));
+	}
+	brunprocess(del, conly, filename, output);
+}
+	/* std::string compile_cmd;
 
 	if (filename.find(".cpp") != std::string::npos) {
 		compile_cmd = "g++ " + filename + " -o" + output;
@@ -33,5 +92,5 @@ int main(int argc, char* argv[]) {
 	std::cout << "brun: Running...\n\n";
 	system(("./" + output).c_str());
 
-	return 0;
-}
+	return 0; */
+
