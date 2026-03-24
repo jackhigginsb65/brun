84fa02360621b7cce0a25f22731e5fd059566d5e
Jack Higgins <jackhigginsb65@gmail.com>
2026-03-23 17:08:27 -0700

    Better file type detection. Improved [brun] messages clarity. Fixed bug where -h wouldn't be detected.

--- brun.cpp
+++ brun.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <filesystem>
 
-#define BRUN_MSG "\033[31m[brun]\033[0m "
+#define BRUN_MSG "\033[1;31m[brun]\033[0;0m "
+#define ERROR "\033[31m[ERROR]\033[0m "
+
+const std::string VERSION = "2.3.2";
 
 int run(const std::string& cmd, bool verbose) {
 	if (verbose) {
@@ -12,40 +16,52 @@ int run(const std::string& cmd, bool verbose) {
 }
 
 int brunprocess(bool del, bool conly, bool verbose, const std::string& filename) {
-	std::string compile_cmd, output;
+	std::string compile_cmd, output, compiler;
 
 	if (filename.find(".") == std::string::npos) {
-		std::cout << BRUN_MSG << "Error; Invalid filename '" << filename << "'. Check spelling?\n";
+		std::cout << BRUN_MSG << ERROR << "Invalid filename '" << filename << "'. Check spelling?\n";
 		return 1;
 	} else {
 		output = filename.substr(0, filename.find_last_of('.'));
 	}
 
-	if (filename.size() >= 4 && filename.substr(filename.size() - 4) == ".cpp" || filename.substr(filename.size() - 4) == ".cxx" || filename.substr(filename.size() - 3) == ".cc") {
+	if (
+		filename.size() >= 4 &&
+		(
+			filename.substr(filename.size() - 4) == ".cpp" ||
+        		filename.substr(filename.size() - 4) == ".cxx" ||
+        		filename.substr(filename.size() - 3) == ".cc"
+		)
+	) {
 		compile_cmd = "g++ " + filename + " -o " + output;
+		compiler = "g++";
 	} else if (filename.size() >= 2 && filename.substr(filename.size() - 2) == ".c") {
 		compile_cmd = "gcc " + filename + " -o " + output;
+		compiler = "gcc";
 	} else {
-		std::cout << BRUN_MSG << "Source-code is not a C/C++ file\n";
+		std::cout << BRUN_MSG << ERROR << "Source-code is not a C/C++ file\n";
 		return 1;
 	}
 
-	std::cout << BRUN_MSG << "Compiling...\n";
+	std::cout << BRUN_MSG << "Compiling " << filename << " with " << compiler << "\n";
 	int compile_result = run(compile_cmd, verbose);
 
 	if (compile_result != 0) {
-		std::cout << BRUN_MSG << "Compilation failed.\n";
+		std::cout << BRUN_MSG << ERROR << "Compilation failed (see compiler output above)\n";
 		return 1;
 	}
 
+	if (verbose) {
+		std::cout << BRUN_MSG << "Output executable: " << output << "\n";
+	}
 	if (del) {
-		std::cout << BRUN_MSG << "Running...\n";
+		std::cout << BRUN_MSG << "Running " << output << "\n";
 		run("./" + output, verbose);
-		run("rm " + output, verbose);
+		std::filesystem::remove(output);
 		return 0;
 	}
 	if (!conly) {
-		std::cout << BRUN_MSG << "Running...\n";
+		std::cout << BRUN_MSG << "Running " << output << "\n";
 		run("./" + output, verbose);
 	}
 	return 0;
@@ -82,7 +98,6 @@ int main(int argc, char* argv[]) {
 	}
 	bool del = false, conly = false, verbose = false;
 	
-	const std::string version = "2.3.2";
 	std::string filename;
 
 	for (int i = 1; i < argc; i++) {
@@ -92,7 +107,7 @@ int main(int argc, char* argv[]) {
 			help();
 			return 0;
 		} else if (arg == "--version") {
-			std::cout << BRUN_MSG << "version " << version << "\n";
+			std::cout << BRUN_MSG << "version " << VERSION << "\n";
 			return 0;	
 		} else if (arg == "-c" || arg == "-C") {
 			conly = true;
@@ -108,5 +123,9 @@ int main(int argc, char* argv[]) {
 		std::cout << BRUN_MSG << "No file specified\n";
 		return 1;
 	}
-	brunprocess(del, conly, verbose, filename);
+	if (conly && del) {
+		std::cout << BRUN_MSG << ERROR << "-c and -d cannot be used together.\n";
+		return 1;
+	}
+	return brunprocess(del, conly, verbose, filename);
 }
